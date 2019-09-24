95687c2b4899bc7d432cfaf8474adc2f56f97db3
Abby Thompson <abby.thompson@myob.com>
2019-09-24 14:59:01 +1200

    adding location object

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,80 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace ConsoleApp1
 {
     internal static class Program
     {
-        private static void Main()
+        private static readonly List<Location> Locations = new List<Location>
         {
-            var locations = new Dictionary<string, Tuple<string, string>>
-            {
-                {"melbourne", new Tuple<string, string>("-37.814", "144.96332")},
-                {"auckland", new Tuple<string, string>("-36.86667", "174.76667")},
-                {"sydney", new Tuple<string, string>("-33.86785", "151.20732")}
-            };
-            
+            new Location("melbourne","-37.814", "144.96332"), 
+            new Location("auckland", "-36.86667", "174.76667"),
+            new Location("sydney","-33.86785", "151.20732")
+        };
+        
+        private static void Main()
+        { 
             var server = new HttpListener();
             server.Prefixes.Add("http://localhost:8080/");
             server.Start();
  
             while (true)
             {
-                var context = server.GetContext(); // Gets the request 
-                var queryString = context.Request.QueryString;
-                var city = queryString["address"];
-
-                var response = context.Response; // Create the Response
-                byte[] buffer;
-
-
-                if (context.Request.HttpMethod == "GET")
-                {
-                    if (!IsCorrectPath(context))
-                    {
-                        response.StatusCode = 400;
-                        buffer = GetErrorBuffer(response.StatusCode);
-                    }
-                    else
-                    {
-                        if (locations.ContainsKey(city))
-                        { 
-                            response.StatusCode = 200;
-                            response.ContentType = "application/json"; 
-                            buffer = GetSuccessBuffer(city, locations);
-                        }
-                        else
-                        {
-                            response.StatusCode = 404;
-                            buffer = GetErrorBuffer(response.StatusCode);
-                        }
-                    }
-                }
-                else
-                {
-                    response.StatusCode = 405;
-                    buffer = GetErrorBuffer(response.StatusCode);
-                }
+                var context = server.GetContext(); // Gets the request   
+                context.Response.ContentType = "application/json";
+                var buffer = Buffer(context);
 
                 context.Response.ContentLength64 = buffer.Length;
                 context.Response.OutputStream.Write(buffer, 0, buffer.Length);
             }
         }
 
-        private static byte[] GetSuccessBuffer(string city, Dictionary<string, Tuple<string, string>> locations)
+        private static byte[] Buffer(HttpListenerContext context)
         {
-            var (latitude, longitude) = locations[city];
+            switch (context.Request.HttpMethod)
+            {
+                case "GET" when !IsCorrectPath(context):
+                    context.Response.StatusCode = 400;
+                    break;
+                case "GET" when Locations.Any(l => l.Name == context.Request.QueryString["address"]):
+                    context.Response.StatusCode = 200;
+                    break;
+                case "GET":
+                    context.Response.StatusCode = 404;
+                    break;
+                case "POST" when !IsCorrectPath(context):
+                    context.Response.StatusCode = 400;
+                    break;
+//                case "POST":
+//                {
+////                    var loc = new Tuple<string, string>(
+////                        context.Request.QueryString["latitude"], 
+////                        context.Request.QueryString["longitude"]);
+////
+////                    Locations.Add(context.Request.QueryString["address"], loc); 
+////                    
+////                    return System.Text.Encoding.UTF8.GetBytes("location added to locations");
+//                }
+                    
 
-            var response = $"{{ \"location\": {city}, \"latitude\" : {latitude}, \"longitude\" : {longitude} }}";
-            
-            return System.Text.Encoding.UTF8.GetBytes(response);
-        }
+                default:
+                    context.Response.StatusCode = 405;
+                    break;
+            }
 
-        private static byte[] GetErrorBuffer(int statusCode)
+            return GetBuffer(context);
+        } 
+
+        private static byte[] GetBuffer(HttpListenerContext context)
         {
             var response = "";
-
+            var statusCode = context.Response.StatusCode; 
+            var loc = context.Request.QueryString["address"];
+            
             switch (statusCode)
             {
                 case 400:
@@ -86,17 +84,32 @@ namespace ConsoleApp1
                 case 405:
                     response = $"Error {statusCode}: Method Not Allowed";
                     break;
-            }
-
+                case 200:
+                    var location = Locations.First(l => l.Name == loc);
+                    response = $"{{ \"location\": {location.Name}, \"latitude\" : {location.Latitude}, \"longitude\" : {location.Longitude} }}";
+                    break;
+            } 
+            
             return System.Text.Encoding.UTF8.GetBytes(response);
         }
 
         private static bool IsCorrectPath(HttpListenerContext context)
         {
-            var path = context.Request.RawUrl.Split("/");
-            if (path[1] != "geocode") return false;
-            var secondHalf = path[2].Split("?");
-            return secondHalf[0] == "json";
+            return context.Request.RawUrl.StartsWith("/geocode/json");
+        }
+    }
+
+    internal class Location
+    {
+        public string Name { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
+        public Location(string name, string latitude, string longitude)
+        {
+            Name = name;
+            Latitude = latitude;
+            Longitude = longitude;
         }
     }
 }
